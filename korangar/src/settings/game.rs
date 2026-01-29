8910dbdd2b98cde8b1f1172d7fba6f6db540d2ac
Wili <museti22@gmail.com>
2026-01-29 22:13:04 +0100

    feat(settings): Add archive_encoding to GameSettings

---
+++ korangar/src/settings/game.rs
@@ -0,0 +1,76 @@
+#[cfg(feature = "debug")]
+use korangar_debug::logging::{Colorize, print_debug};
+use korangar_interface::element::StateElement;
+use ron::ser::PrettyConfig;
+use rust_state::RustState;
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone, Copy, Debug, Serialize, Deserialize, RustState, StateElement, PartialEq, Eq)]
+pub enum Encoding {
+    EucKr,
+    Windows1252,
+}
+
+impl Default for Encoding {
+    fn default() -> Self {
+        Self::EucKr
+    }
+}
+
+#[derive(Clone, Serialize, Deserialize, RustState, StateElement)]
+pub struct GameSettings {
+    pub auto_attack: bool,
+    #[serde(default)]
+    pub archive_encoding: Encoding,
+}
+
+impl Default for GameSettings {
+    fn default() -> Self {
+        Self {
+            auto_attack: true,
+            archive_encoding: Encoding::default(),
+        }
+    }
+}
+
+impl GameSettings {
+    const FILE_NAME: &'static str = "client/game_settings.ron";
+
+    pub fn new() -> Self {
+        Self::load().unwrap_or_else(|| {
+            #[cfg(feature = "debug")]
+            print_debug!("failed to load game settings from {}", Self::FILE_NAME.magenta());
+            Default::default()
+        })
+    }
+
+    pub fn load() -> Option<Self> {
+        #[cfg(feature = "debug")]
+        print_debug!("loading game settings from {}", Self::FILE_NAME.magenta());
+        std::fs::read_to_string(Self::FILE_NAME)
+            .ok()
+            .and_then(|data| ron::from_str(&data).ok())
+    }
+
+    pub fn save(&self) {
+        #[cfg(feature = "debug")]
+        print_debug!("saving game settings to {}", Self::FILE_NAME.magenta());
+
+        let data = ron::ser::to_string_pretty(self, PrettyConfig::new()).unwrap();
+
+        if let Err(_error) = std::fs::write(Self::FILE_NAME, data) {
+            #[cfg(feature = "debug")]
+            print_debug!(
+                "failed to save game settings to {}: {:?}",
+                Self::FILE_NAME.magenta(),
+                _error.red()
+            );
+        }
+    }
+}
+
+impl Drop for GameSettings {
+    fn drop(&mut self) {
+        self.save();
+    }
+}
